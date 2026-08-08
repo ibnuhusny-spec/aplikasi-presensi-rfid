1a5650d14e44b64f4a3de11d1ea8274aea15dc4c
Ibnu Husny <ibnuhusny@gmail.com>
2026-08-08 17:39:54 +0800

    Add no-cache meta tags and SafeErrorBoundary auto recovery

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom/client'
 import AplikasiPresensi from './AplikasiPresensi.jsx'
 import './index.css'
 
+class SafeErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("SafeErrorBoundary caught error:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      try {
+        localStorage.removeItem('presensi_school_settings');
+        localStorage.removeItem('presensi_mock_pengguna_list');
+      } catch (e) {}
+      return <AplikasiPresensi />;
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AplikasiPresensi />
+    <SafeErrorBoundary>
+      <AplikasiPresensi />
+    </SafeErrorBoundary>
   </React.StrictMode>,
 )
